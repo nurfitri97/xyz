de5c7f4bcb89ad1bad22ea72d26d6806bdf3867e
Fitri Halim <mohamad.nur.fitri.abd.halim@wdc.com>
2023-02-16 10:01:47 +0800

    Add viewmodels

---
+++ XyzSystem/ViewModels/ProductViewModel.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using XyzSystem.Models.Domain;
+
+namespace XyzSystem.ViewModels
+{
+    public class ProductViewModel
+    {
+        public Product Product { get; set; } = new();
+        public IEnumerable<SelectListItem>? AllCategories { get; set; }
+
+        private List<int>? _selectedCategories;
+        public List<int>? SelectedCategories
+        {
+            get
+            {
+                if (_selectedCategories == null)
+                {
+                    _selectedCategories = Product?.Categories?.Select(m => m.CategoryId).ToList();
+                }
+                return _selectedCategories;
+            }
+            set { _selectedCategories = value; }
+        }
+    }
+}
